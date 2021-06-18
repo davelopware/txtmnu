b396979504046c396dea31fe289090ccb5e35446
Dave Amphlett <dave@davelopware.com>
2021-06-18 14:03:27 +0100

    Added MenuEntryWithData

---
+++ Davelopware/TxtMnu/MenuEntryWithData.cs
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2007 Davelopware Ltd
+ * 
+ * http://www.davelopware.com/txtmnu/
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+using System;
+
+namespace Davelopware.TxtMnu
+{
+	/// <summary>
+	/// The simplest implementation of IMenuEntry
+	/// </summary>
+	public class MenuEntryWithData<T> : IMenuEntry
+	{
+		private const string SESSION_KEY_SESSION_VISIBLE = "SessionVisible";
+
+		private string _key = string.Empty;
+		private string _name = string.Empty;
+		private string _help = string.Empty;
+		private T _data;
+		private bool _visible = true;
+
+		#region constructors
+
+		public MenuEntryWithData()
+		{
+		}
+
+		public MenuEntryWithData(string key, string name, T data)
+		{
+			_key = key;
+			_name = name;
+			_data = data;
+		}
+
+		#endregion
+
+		#region IMenuEntry Members
+
+		public event Davelopware.TxtMnu.MenuEntrySelectedHandler MenuEntrySelected;
+
+		public virtual string Key
+		{
+			get { return _key; }
+			set { _key = value; }
+		}
+
+		public virtual string GetName(MenuSession session, Menu menu)
+		{
+			return _name;
+		}
+
+		public virtual string Name
+		{
+			set { _name = value; }
+		}
+
+		public virtual string Help
+		{
+			get { return _help; }
+			set { _help = value; }
+		}
+
+		public virtual bool Visible
+		{
+			get { return _visible; }
+			set { _visible = value; }
+		}
+
+		public bool GetSessionVisible(MenuSession session)
+		{
+			object val = session.GetSessionData(SessionKey(SESSION_KEY_SESSION_VISIBLE));
+			if (val == null)
+				return true; // default is that the entry is visible
+
+			return ((bool)val == true);
+		}
+
+		public void SetSessionVisible(MenuSession session, bool visible)
+		{
+			session.SetSessionData(SessionKey(SESSION_KEY_SESSION_VISIBLE), visible);
+		}
+
+		public virtual bool KeyCompare(string input)
+		{
+			return (_key.CompareTo(input) == 0);
+		}
+
+		public virtual void FireMenuEntrySelected(MenuSession session, Menu menu)
+		{
+			if (MenuEntrySelected != null)
+				MenuEntrySelected(this, session, menu);
+		}
+
+		#endregion
+
+		public T getData()
+        {
+			return _data;
+        }
+
+		public void setData(T data)
+        {
+			_data = data;
+        }
+
+		private string SessionKey(string innerKey)
+		{
+			return this.GetHashCode() + "mes" + innerKey;
+		}
+	}
+}
